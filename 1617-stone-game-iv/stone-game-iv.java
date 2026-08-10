d689a72d37cebba460eda6c5cb2c922b79fbaf1f
Narendra Saraf <narendrasaraf16@gmail.com>
2026-08-10 12:33:50 +0530

    Time: 3 ms (99.74%) | Memory: 42.5 MB (78.33%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,15 +1,12 @@
 class Solution {
-    static final int MAX = 100000;
-    static final boolean[] dp = new boolean[MAX + 1];
-
-    static {
-        for (int i = 0; i <= MAX; i++) {
-            if (dp[i]) continue;
-
-            for (int j = 1; j * j <= MAX - i; j++)
-                dp[i + j * j] = true;
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) {
+            if (!dp[i]) {
+                for (int j = 1; i + j * j <= n; j++) dp[i + j * j] = true;
+                if (dp[n]) return true;
+            }
         }
+        return false;
     }
-
-    public boolean winnerSquareGame(int n) { return dp[n]; }
 }
